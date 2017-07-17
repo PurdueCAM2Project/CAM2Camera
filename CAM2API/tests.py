8481791b834a8eaad262d5b2ec556f0725e6cfd4
mmalesev <malesevic.milos2@gmail.com>
2017-07-17 19:29:29 -0400

    Preparing tests for merging authentication code

--- CAM2API/tests.py
+++ CAM2API/tests.py
@@ -19,7 +19,13 @@ import json
 import os
 import requests
 
-class API_View_Tests(APITestCase):
+class CAM2APITest(APITestCase):
+
+    def send_post_request(self, url, data):
+        response = self.client.post(url, data, format='json')
+        return response
+
+class AddingNewCameraTests(CAM2APITest):
 
     def setUp(self):
 
@@ -29,43 +35,41 @@ class API_View_Tests(APITestCase):
                               'url': 'https://www.google.com/', 'camera_id': 2}
 
     def test_post_IPCamera_Accepted(self):
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.assertEqual(response.status_code, requests.codes.created)
 
     def test_post_NonIPCamera_Accepted(self):
-        response = self.client.post('/cameras/', self.non_ip_camera,
-                                    format='json')
+        response = self.send_post_request('/cameras/', self.non_ip_camera)
         self.assertEqual(response.status_code, requests.codes.created)
 
     def test_post_IPCameraMissingIP_Rejected(self):
         del self.ip_camera['ip']
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.assertEqual(response.status_code, requests.codes.bad_request)
 
     def test_post_NonIPCameraMissingURL_Rejected(self):
         del self.non_ip_camera['url']
-        response = self.client.post('/cameras/', self.non_ip_camera,
-                                    format='json')
+        response = self.send_post_request('/cameras/', self.non_ip_camera)
         self.assertEqual(response.status_code, requests.codes.bad_request)
 
     def test_post_IPCameraMissingID_Rejected(self):
         del self.ip_camera['camera_id']
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.assertEqual(response.status_code, requests.codes.bad_request)
 
     def test_post_MissingLatitude_Rejected(self):
         del self.ip_camera['lat']
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.assertEqual(response.status_code, requests.codes.bad_request)
 
     def test_post_MissingLongitude_Rejected(self):
         del self.ip_camera['lng']
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.assertEqual(response.status_code, requests.codes.bad_request)
 
     def test_post_WrongCityCorrection_Accepted(self):
         self.ip_camera['city'] = 'IncorrectCity'
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.assertEqual(response.status_code, requests.codes.created)
         added_camera = Camera.objects.get(camera_id=self.ip_camera['camera_id'])
         self.assertEqual(added_camera.city, 'Shinjuku-ku')
@@ -73,7 +77,7 @@ class API_View_Tests(APITestCase):
     def test_post_MissingCityCantBeDetected_Accepted(self):
         self.ip_camera['lat'] = 30.0443
         self.ip_camera['lng'] = 31.34
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.assertEqual(response.status_code, requests.codes.created)
         city = Camera.objects.get(camera_id=self.ip_camera['camera_id']).city
         self.assertEqual(city, None)
@@ -81,41 +85,41 @@ class API_View_Tests(APITestCase):
     def test_post_CountryDetection_Accepted(self):
         self.ip_camera['lat'] = 30.0443
         self.ip_camera['lng'] = 31.34
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         country = Camera.objects.get(lat=self.ip_camera['lat']).country
         self.assertEqual(country, 'Egypt')
 
     def test_post_StateDetection_Accepted(self):
         self.ip_camera['lat'] = 40.4259
         self.ip_camera['lng'] = -86.9081
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         state = Camera.objects.get(lat=self.ip_camera['lat']).state
         self.assertEqual(state, 'IN')
 
     def test_post_DuplicateCameraID_Rejected(self):
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.ip_camera['ip'] = '10.0.0.2'
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.assertEqual(response.status_code, requests.codes.bad_request)
 
     def test_post_DuplicateIP_Rejected(self):
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.ip_camera['camera_id'] = 11
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.assertEqual(response.status_code, requests.codes.bad_request)
 
     def test_post_DuplicateURL_Rejected(self):
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.non_ip_camera['camera_id'] = 11
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.assertEqual(response.status_code, requests.codes.bad_request)
 
     def test_post_LatitudeExceedsLimits_Rejected(self):
         self.ip_camera['lat'] = 91
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.assertEqual(response.status_code, requests.codes.bad_request)
 
     def test_post_LongitudeExceedsLimits_Rejected(self):
         self.ip_camera['lng'] = -181
-        response = self.client.post('/cameras/', self.ip_camera, format='json')
+        response = self.send_post_request('/cameras/', self.ip_camera)
         self.assertEqual(response.status_code, requests.codes.bad_request)
