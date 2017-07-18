39cbeda8789f34d95816428105c507b7ed214e4f
mmalesev <malesevic.milos2@gmail.com>
2017-07-18 00:44:44 -0400

    Allowing negative numbers for lat/lon in query url

--- CAM2API/urls.py
+++ CAM2API/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from CAM2API import views
 
-float_num = r'\d+\.?\d{0,4}'
+float_num = r'\-?\d+\.?\d{0,4}'
 urlpatterns = [
 	url(r'^$', views.Home.as_view()),
 	url(r'^cameras/$', views.CameraList.as_view()),
 	url(r'^(?P<cd>\d+)/$', views.CameraByID.as_view()),
-	url(r'^query/lat=(?P<lat>{})\,lon=(?P<lon>{}),(?P<query_type>(radius|count))=(?P<value>\d+)/$'.format(float_num, float_num),views.CameraQuery.as_view()),
+	url(r'^query/lat=(?P<lat>{})\,lng=(?P<lon>{}),(?P<query_type>(radius|count))=(?P<value>\d+)/$'.format(float_num, float_num),views.CameraQuery.as_view()),
 ]
 
 # Adds format patterns to the API
